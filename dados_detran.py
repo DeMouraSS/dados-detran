942cba01059ef50945fae1d4d76036e5fb6aba2c
Lucas de Moura <lucasdemouraup@gmail.com>
2021-07-20 11:42:18 -0300

    agora vai

--- dados_detran.py
+++ dados_detran.py
@@ -1,5 +1,7 @@
 import csv
+from unidecode import unidecode
 with open('dados-abertos-maio.csv', newline='') as csvfile:
     arquivo = csv.reader(csvfile, delimiter=';')
     for linha in arquivo:
-        print(linhgita[3].lower())
+        remover_acentos = unidecode(linha[3].lower())
+        print(remover_acentos)
