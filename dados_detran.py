051fa2c69c7cfc0a7e653560bf2a4ec5b67117b4
Lucas de Moura <lucasdemouraup@gmail.com>
2021-07-20 10:29:37 -0300

    agora vai

---
+++ dados_detran.py
@@ -0,0 +1,5 @@
+import csv
+with open('dados-abertos-maio.csv', newline='') as csvfile:
+    arquivo = csv.reader(csvfile, delimiter=';')
+    for linha in arquivo:
+        print(linhgita[3].lower())
