7f45c46d5b750b190e540865d2e4853414a7d30d
Lucas de Moura <lucasdemouraup@gmail.com>
2021-08-10 10:10:03 -0300

    atividade de terça passada

--- dados_detran.py
+++ dados_detran.py
@@ -1,7 +1,12 @@
 import csv
+from collections import Counter
 from unidecode import unidecode
-with open('dados-abertos-maio.csv', newline='') as csvfile:
+tipos_veiculos = list()
+with open('dados-abertos-maio.csv') as csvfile:
     arquivo = csv.reader(csvfile, delimiter=';')
     for linha in arquivo:
-        remover_acentos = unidecode(linha[3].lower())
-        print(remover_acentos)
+        tipo_veiculo = unidecode(linha[3].lower().strip())
+        tipos_veiculos.append(tipo_veiculo)
+contador_de_veiculos = Counter(tipos_veiculos)
+for chaver, valor in contador_de_veiculos.items():
+    print(f"{chaver} - {valor}")
