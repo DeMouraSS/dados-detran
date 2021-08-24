6217f342290e033cdbad294956f281c8ade40991
Lucas De Moura <87019816+DeMouraSS@users.noreply.github.com>
2021-08-23 21:45:02 -0300

    não conseguir!

--- dados_detran.py
+++ dados_detran.py
@@ -3,6 +3,7 @@ from collections import Counter
 from unidecode import unidecode
 import os
 tipos_veiculos = list()
+tipos_infracoes = list()
 tipo_arquivo = list()
 pasta ='/home/lucas/PycharmProjects/dados-detran1/relatorios'
 lista_arquivos = os.listdir(pasta)
@@ -10,11 +11,17 @@ arquivos = os.scandir(pasta)
 for t in arquivos:
     print(t.name)
 print('==' * 50)
-with open('relatorios/arquivo(4).csv') as csvfile:
+with open('relatorios/arquivo(3).csv') as csvfile:
     arquivo = csv.reader(csvfile, delimiter=';')
     for linha in arquivo:
+        tipo_infracao = linha[12]
         tipo_veiculo = unidecode(linha[3].lower().strip())
         tipos_veiculos.append(tipo_veiculo)
+        tipos_infracoes.append(tipo_infracao)
 contador_de_veiculos = Counter(tipos_veiculos)
+contador_de_infracoes = Counter(tipos_infracoes)
 for chaver, valor in contador_de_veiculos.items():
     print(f"{chaver} - {valor}")
+print('~' * 50)
+for coluna, v in contador_de_infracoes.items():
+    print(f"{coluna} - {v}")
