6e7cf044bda5922fe9d71986d21db05a7be60e42
Lucas de Moura <lucasdemouraup@gmail.com>
2021-08-31 11:33:20 -0300

    aula do dia 31

--- dados_detran.py
+++ dados_detran.py
@@ -7,31 +7,58 @@ arquivo_excel = Workbook()
 tipos_veiculos = list()
 tipos_infracoes = list()
 tipo_arquivo = list()
-contador = 1
-pasta ='/Users/aman.rathie/PycharmProjects/detrans/relatorios'
-lista_arquivos = os.listdir(pasta)
-arquivos = os.scandir(pasta)
-for t in arquivos:
-    print(t.name)
-    print('==' * 50)
-    nome_arquivo = 'relatorios/' + t.name
-    with open(nome_arquivo) as csvfile:
-        arquivo = csv.reader(csvfile, delimiter=';')
-        for linha in arquivo:
-            tipo_infracao = linha[12]
-            tipo_veiculo = unidecode(linha[3].lower().strip())
-            tipos_veiculos.append(tipo_veiculo)
-            tipos_infracoes.append(tipo_infracao)
-    contador_de_veiculos = Counter(tipos_veiculos)
-    contador_de_infracoes = Counter(tipos_infracoes)
-    #del contador_de_veiculos['tipo_veiculo']
-    planilha1 = arquivo_excel.create_sheet(t.name)
+def relatorios(pasta):
+    arquivos = os.listdir(pasta)
+    arquivos.sort()
+    print(arquivos)
+    return arquivos
+
+
+def criar_planilha(contador_de_veiculos, contador_de_infracoes, arquivo):
+    contador = 1
+    planilha1 = arquivo_excel.create_sheet(arquivo)
+    planilha1.cell(row=1, column=1, value='tipos de veiculos')
+    planilha1.cell(row=1, column=2, value='Multas')
     for tipo_v, valor in contador_de_veiculos.items():
         print(f"{tipo_v} - {valor}")
         contador += 1
         planilha1.cell(row=contador, column=1, value=tipo_v)
         planilha1.cell(row=contador, column=2, value=valor)
-    print('~' * 50)
+    print('-=' * 50)
     for coluna, v in contador_de_infracoes.items():
         print(f"{coluna} - {v}")
+    print('''
+        
+    ''')
+
+
+def obter_tipo_infracao(linha):
+    tipo_infracao = linha[12]
+    return tipo_infracao
+
+
+def obter_tipo_de_veiculo(linha):
+    tipo_veiculo = unidecode(linha[3].lower().strip())
+    return tipo_veiculo
+
+
+for arquivo in relatorios('/Users/aman.rathie/PycharmProjects/detrans/relatorios'):
+    print()
+    print('==' * 50)
+    print(arquivo)
+    print('==' * 50)
+    nome_arquivo = 'relatorios/' + arquivo
+    with open(nome_arquivo) as csvfile:
+        csv_file = csv.reader(csvfile, delimiter=';')
+        for linha in csv_file:
+            tipo_infracao = obter_tipo_infracao(linha)
+            tipo_veiculo = obter_tipo_de_veiculo(linha)
+            tipos_veiculos.append(tipo_veiculo)
+            tipos_infracoes.append(tipo_infracao)
+    contador_de_veiculos = Counter(tipos_veiculos)
+    contador_de_infracoes = Counter(tipos_infracoes)
+    del contador_de_veiculos['tipo_veiculo']
+    criar_planilha(contador_de_veiculos, contador_de_infracoes, arquivo)
+
+del arquivo_excel['Sheet']
 arquivo_excel.save("relatorio.xlsx")
